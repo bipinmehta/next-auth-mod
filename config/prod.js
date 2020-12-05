d101693926dc7f3c54f6d89516eaa707a8429cbf
Bipin Mehta <bipinmehta289@gmail.com>
2020-12-05 16:40:44 +0530

    Initial commit

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  MONGO_URI:
+    'mongodb+srv://bipin:narayan1@cluster0-0y4aa.mongodb.net/home-kitchen?retryWrites=true&w=majority', // MongoDB's URI goes here
+  SECRET: 'Neeta1&)@', // SECRET key goes here
+};
+
+// module.exports = {
+//   MONGO_URI: process.env.MONGO_URI,
+//   SECRET: process.env.SECRET,
+// };
